cda0c1eb50c0eaa2383b7339e969de0518b0a748
muh-younus <braq369@gmail.com>
2024-12-08 13:48:56 +0500

    anathor commit

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
-let imgList = document.querySelectorAll(".imgSlide");
-let counter = 0;
+let leftArrow = document.querySelector(".leftArrow");
+let rightArrow = document.querySelector(".rightArrow");
+let slider = document.querySelector(".slider");
+let image = document.querySelectorAll(".image");
+console.log(image.length);
 
-// Set initial positions for slides
-imgList.forEach((slide, index) => {
-    slide.style.left = `${index * 100}%`;
+let slideNumber = 1;
+let length = image.length;
+
+// Next-Slide
+const nextArrow = () => {
+
+    slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+    slideNumber++;
+}
+// First-slide
+const firstSlide = () => {
+
+    slider.style.transform = `translateX(-${0}px)`;
+    slideNumber = 1;
+
+
+}
+
+// Right-arrow-EventListener
+rightArrow.addEventListener("click", () => {
+
+    slideNumber < length ? nextArrow() : firstSlide();
+    
 });
 
-let imgSlide1 = () => {
-    // Move all slides based on the counter value
-    imgList.forEach((slide) => {
-        slide.style.transform = `translateX(-${counter * 100}%)`;
-    });
-};
+// Left-Arrow-Function-Method
 
-let goNext = () => {
-    // Prevent counter from exceeding the number of slides
-    if (counter < imgList.length - 1) {
-        counter++;
-        imgSlide1();
-    }
-};
+const prevArrow = () => {
 
-let goPrev = () => {
-    // Prevent counter from going below 0
-    if (counter > 0) {
-        counter--;
-        imgSlide1();
-    }
-};
+    slider.style.transform = `translateX(-${(slideNumber-2) * 800}px)`;
+    slideNumber--;
+}
+//Last-slide
+const lastSlide = () => {
+
+    slider.style.transform = `translateX(-${(length-1)*800}px)`;
+    slideNumber = length;
+
+
+}
+
+leftArrow.addEventListener("click", () => {
+
+    slideNumber  > 1 ? prevArrow() : lastSlide()
+    
+});
